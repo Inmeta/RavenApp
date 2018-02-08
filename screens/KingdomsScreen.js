4ef63efdf295af092fa1f621736f769804a1e9ea
Mohamed Ali Abdi <mohamed.abdi@inmeta.no>
2018-02-08 19:37:03 +0100

    #1 added gifted

--- screens/KingdomsScreen.js
+++ screens/KingdomsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import { Tenants } from './../api/tenants';
 export default class KingdomsScreen extends React.Component {
   static navigationOptions = {
@@ -12,11 +12,14 @@ export default class KingdomsScreen extends React.Component {
 
         <View><Text>Test</Text></View>
         {Tenants.map(tenant => {
-          return <View key={tenant.id} style={{padding: 10, marginBottom: 10}}>
-            <Text>{tenant.name}</Text>
-          </View>
+          return (
+          <TouchableOpacity key={tenant.id}  style={{padding: 10, marginBottom: 10}}>
+            <View>
+              <Text>{tenant.name}</Text>
+            </View>
+          </TouchableOpacity>
+          );
         })}
-
       </ScrollView>
     );
   }
